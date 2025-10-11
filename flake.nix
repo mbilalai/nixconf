8f0f5831e7d0fdeca43ef3f3897acb7a67234838
mbilalai <mbilalai@protonmail.ch>
2025-10-11 21:15:34 +1100

    nix+cosmicDE

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "nixos flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+  in
+    {
+
+    nixosConfigurations = {
+      alpha = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+  };
+}
