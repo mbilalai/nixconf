bdad4110ee775359dd5458ef53874b77975f7c46
mbilalai <mbilalai@protonmail.ch>
2026-02-23 15:05:02 +1100

    Configure git with SSH, difftastic, LFS, and signing

--- home/common/git.nix
+++ home/common/git.nix
@@ -8,6 +8,40 @@
     extraConfig = {
       init.defaultBranch = "main";
       core.editor = "helix";
+      core.sshCommand = "ssh -o AddKeysToAgent=yes";
+    };
+
+    lfs.enable = true;
+
+    difftastic.enable = true;
+
+    ignores = [
+      ".DS_Store"
+      "*.swp"
+      "*~"
+      ".env"
+      ".env.local"
+    ];
+
+    signing = {
+      signByDefault = true;
+      key = "mbilalai@protonmail.ch";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        host = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "gitlab.com" = {
+        host = "gitlab.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
     };
   };
 }
