bb162e6b44526442c78fccde5192f714eb245765
mbilalai <mbilalai@protonmail.ch>
2026-02-23 15:43:21 +1100

    Auto-hide menu bar

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -30,6 +30,7 @@
         KeyRepeat = 2;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
+        _NSMainMenuNibSDKVersion = 12; # Auto-hide menu bar
       };
 
       # Trackpad settings
